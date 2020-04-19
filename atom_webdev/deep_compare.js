af9957de1e29d730a15966fcc2627eb574541c3e
fmj23 <60016324+fmj23@users.noreply.github.com>
2020-04-18 22:18:39 -0500

    save

--- atom_webdev/deep_compare.js
+++ atom_webdev/deep_compare.js
@@ -5,8 +5,11 @@ function deepEqual(value1,value2)
 }
 console.log("Homework tests:");
 let obj = {here: {is: "an"}, object: 2};
-console.log(deepEqual(obj,obj));
+//console.log(deepEqual(obj,obj));
+(deepEqual(obj,obj));
 
-console.log(deepEqual(obj,{here:1,object:2}));
+//console.log(deepEqual(obj,{here:1,object:2}));
+(deepEqual(obj,{here:1,object:2}));
 
-console.log(deepEqual(obj,{here:{is:"an"}, object:2}));
+//console.log(deepEqual(obj,{here:{is:"an"}, object:2}));
+(deepEqual(obj,{here:{is:"an"}, object:2}));
